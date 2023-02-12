fca266048061111749c0b9e2534e4a7c6f448f87
Stephen Cardie <das.shaggy@gmail.com>
2023-02-12 11:44:10 -0500

    updating with new code

--- assets/scripts/svg.js
+++ assets/scripts/svg.js
@@ -1,4 +1,28 @@
-const svg = require('svg');
+const SVG = require('svg.js');
 
-
+function renderLinkIcon(node) {
+    const iconSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    const iconPath = document.createElementNS(
+      'http://www.w3.org/2000/svg',
+      'path'
+    );
+  
+    iconSvg.setAttribute('fill', 'none');
+    iconSvg.setAttribute('viewBox', '0 0 24 24');
+    iconSvg.setAttribute('stroke', 'black');
+    iconSvg.classList.add('post-icon');
+  
+    iconPath.setAttribute(
+      'd',
+      'M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1'
+    );
+    iconPath.setAttribute('stroke-linecap', 'round');
+    iconPath.setAttribute('stroke-linejoin', 'round');
+    iconPath.setAttribute('stroke-width', '2');
+  
+    iconSvg.appendChild(iconPath);
+  
+    return node.appendChild(iconSvg);
+  }
+  
 module.exports;
